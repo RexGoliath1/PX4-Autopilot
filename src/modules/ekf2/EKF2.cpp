34cb2cc09a53b26014462bd2d07db30b289649fa
RexGoliath1 <sgoncia@gmail.com>
2026-05-07 15:30:10 -0500

    fix(simulation,ekf2): VIO Specific. Remove preflight yaw_align innovation failure logic and replace with arm transition warning

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -1987,7 +1987,34 @@ void EKF2::PublishStatus(const hrt_abstime &timestamp)
 	status.time_slip = _last_time_slip_us * 1e-6f;
 
 	static constexpr float kMinTestRatioPreflight = 0.5f;
-	status.pre_flt_fail_innov_heading   = (kMinTestRatioPreflight < status.hdg_test_ratio) || !_ekf.control_status_flags().yaw_align;
+	status.pre_flt_fail_innov_heading   = (kMinTestRatioPreflight < status.hdg_test_ratio);
+
+
+	// Warn when we're trying to arm if no yaw_alignment
+	vehicle_status_s vehicle_status;
+	if (_status_sub.copy(&vehicle_status)) {
+		const bool armed = (vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_ARMED);
+
+		bool just_armed = false;
+		
+		if (_armed_transition && !armed) {
+		    _armed_transition = false;
+		}
+		
+		if (armed && !_armed_transition) {
+		    _armed_transition = true;
+		    just_armed = true;  // only true on the rising edge
+		}
+
+		if (!_ekf.control_status_flags().yaw_align && _armed_transition && just_armed) {
+#if defined(CONFIG_EKF2_EXTERNAL_VISION)
+			PX4_WARN("No yaw_align set for control flags! Bypassing preflight estimation check! EV_CTRL: %d", _param_ekf2_ev_ctrl.get());
+#else // defined(CONFIG_EKF2_EXTERNAL_VISION)
+			PX4_WARN("No yaw_align set for control flags! Bypassing preflight estimation check!");
+#endif
+		}
+	}
+
 	status.pre_flt_fail_innov_height    = (kMinTestRatioPreflight < status.hgt_test_ratio);
 	status.pre_flt_fail_innov_pos_horiz = (kMinTestRatioPreflight < status.pos_test_ratio);
 	status.pre_flt_fail_innov_vel_horiz = (kMinTestRatioPreflight < vel_xy_test_ratio);
